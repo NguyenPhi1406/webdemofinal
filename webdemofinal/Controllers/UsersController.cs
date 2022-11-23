9c319c3016d88677d92e9d122b89cdcc90350d3d
NhomNhem <95702943+NhomNhem@users.noreply.github.com>
2022-11-23 12:56:02 +0700

    Add project files.

---
+++ webdemofinal/Controllers/UsersController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using webdemofinal.Models;
+namespace webdemofinal.Controllers
+{
+    public class UsersController : Controller
+    {
+        AdminC db = new AdminC();
+        // GET: Admin/Login
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(cust.NameCus))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(cust.PassCus))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (string.IsNullOrEmpty(cust.EmailCus))
+                    ModelState.AddModelError(string.Empty, "Email không được để trống");
+                if (string.IsNullOrEmpty(cust.PhoneCus))
+                    ModelState.AddModelError(string.Empty, "Điện thoại không được để trống");
+                //Kiểm tra xem có người nào đã đăng kí với tên đăng nhập này hay chưa
+
+                var khachhang = db.Customers.FirstOrDefault(k => k.NameCus == cust.NameCus);
+                if (khachhang != null)
+                    ModelState.AddModelError(string.Empty, "Đã có người đăng kí tên này");
+                if (ModelState.IsValid)
+                {
+                    db.Customers.Add(cust);
+                    db.SaveChanges();
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Customer cust)
+        {
+            if (ModelState.IsValid)
+            {
+                if (string.IsNullOrEmpty(cust.NameCus))
+                    ModelState.AddModelError(string.Empty, "Tên đăng nhập không được để trống");
+                if (string.IsNullOrEmpty(cust.PassCus))
+                    ModelState.AddModelError(string.Empty, "Mật khẩu không được để trống");
+                if (ModelState.IsValid)
+                {
+                    //Tìm khách hàng có tên đăng nhập và password hợp lệ trong CSDL
+
+
+                    var khachhang = db.Customers.FirstOrDefault(k => k.NameCus == cust.NameCus && k.PassCus == cust.PassCus);
+                    if (khachhang != null)
+                    {
+                        ViewBag.ThongBao = "Chúc mừng đăng nhập thành công";
+                        //Lưu vào session
+                        Session["ID"] = khachhang.IDCus;
+                        Session["TaiKhoan"] = khachhang;
+                        Session["TenTaiKhoan"] = khachhang.NameCus;
+                        return RedirectToAction("ProductList", "ProductCus");
+                    }
+                    else
+                        ViewBag.ThongBao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
